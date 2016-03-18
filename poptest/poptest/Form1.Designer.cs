3932a2e9b48affd7b8807b73c22887daf1d8fb13
Marco <marco853@msn.com>
2016-03-18 19:57:27 +0000

    form2_add

--- poptest/poptest/Form1.Designer.cs
+++ poptest/poptest/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(104, 101);
+            this.comboBox1.Location = new System.Drawing.Point(43, 61);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(108, 21);
             this.comboBox1.TabIndex = 0;
