580fc0ca9a22d7052ae5abd92d42e59054599dd1
Lino <linosoft@hotmail.com>
2016-03-18 20:19:53 +0000

    from2 ok button

--- poptest/poptest/Form2.Designer.cs
+++ poptest/poptest/Form2.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 72);
+            this.button1.Location = new System.Drawing.Point(171, 114);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(263, 82);
+            this.button1.Size = new System.Drawing.Size(61, 32);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form2
